25f0c3452deaf0b2fd84fba203d9bd66325cc523
Edryu <trickyjuggler29@gmail.com>
2025-08-13 14:23:40 -0500

    Initial commit

---
+++ src/client/java/com/edryu/moreblocks/MoreBlocksClient.java
@@ -0,0 +1,10 @@
+package com.edryu.moreblocks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MoreBlocksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/com/edryu/moreblocks/MoreBlocksDataGenerator.java
@@ -0,0 +1,11 @@
+package com.edryu.moreblocks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class MoreBlocksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
