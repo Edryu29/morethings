90884902b9186e08a293f53c1fc466c87e1b5b34
Edryu <trickyjuggler29@gmail.com>
2025-09-07 17:35:38 -0500

    Fixed rope item

--- src/client/java/com/edryu/moreblocks/MoreBlocksModelProvider.java
+++ src/client/java/com/edryu/moreblocks/MoreBlocksModelProvider.java
@@ -17,8 +17,6 @@ public class MoreBlocksModelProvider extends FabricModelProvider {
         blockStateModelGenerator.registerSimpleCubeAll(MoreBlocksRegister.DAUB_BRACE_FLIPPED);
         blockStateModelGenerator.registerSimpleCubeAll(MoreBlocksRegister.DAUB_CROSS_BRACE);
         blockStateModelGenerator.registerSimpleCubeAll(MoreBlocksRegister.DAUB_FRAME);
-
-        blockStateModelGenerator.registerItemModel(MoreBlocksRegister.ROPE);
     }
     
     @Override
