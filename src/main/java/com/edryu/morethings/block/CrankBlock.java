283aa9482d110cd65f91ba681650aab306a27b18
Edryu <trickyjuggler29@gmail.com>
2025-11-12 18:01:44 -0500

    Improved position placement

--- src/main/java/com/edryu/morethings/block/CrankBlock.java
+++ src/main/java/com/edryu/morethings/block/CrankBlock.java
@@ -1,21 +1,71 @@
 package com.edryu.morethings.block;
 
+import org.jetbrains.annotations.Nullable;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.ShapeContext;
+import net.minecraft.block.SideShapeType;
+import net.minecraft.block.TripwireBlock;
+import net.minecraft.block.enums.BlockFace;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.fluid.Fluids;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.IntProperty;
 import net.minecraft.state.property.Properties;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
+import net.minecraft.util.shape.VoxelShape;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.WorldAccess;
+import net.minecraft.world.WorldView;
 
 public class CrankBlock extends WaterloggableBlock {
     public static final DirectionProperty FACING = Properties.FACING;
     public static final IntProperty POWER = Properties.POWER;
 
+    protected static final VoxelShape SHAPE_DOWN = Block.createCuboidShape(2, 11, 2, 14, 16, 14);
+    protected static final VoxelShape SHAPE_UP = Block.createCuboidShape(2, 0, 2, 14, 5, 14);
+    protected static final VoxelShape SHAPE_NORTH = Block.createCuboidShape(2, 2, 11, 14, 14, 16);
+    protected static final VoxelShape SHAPE_SOUTH = Block.createCuboidShape(2, 2, 0, 14, 14, 5);
+    protected static final VoxelShape SHAPE_EAST = Block.createCuboidShape(0, 2, 2, 5, 14, 14);
+    protected static final VoxelShape SHAPE_WEST = Block.createCuboidShape(11, 2, 2, 16, 14, 14);
+
     public CrankBlock(Settings settings) {
         super(settings);
         setDefaultState(getDefaultState().with(WATERLOGGED, false).with(FACING, Direction.NORTH).with(POWER, 0));
     }
+
+    @Override
+    public VoxelShape getOutlineShape(BlockState state, BlockView view, BlockPos pos, ShapeContext context) {
+        return switch (state.get(FACING)) {
+            case NORTH -> SHAPE_NORTH;
+            case WEST -> SHAPE_WEST;
+            case EAST -> SHAPE_EAST;
+            case UP -> SHAPE_UP;
+            case DOWN -> SHAPE_DOWN;
+            default -> SHAPE_SOUTH;
+        };
+    }
+
+	@Nullable
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+        boolean wl = ctx.getWorld().getFluidState(ctx.getBlockPos()).isOf(Fluids.WATER);
+		for (Direction direction : ctx.getPlacementDirections()) {
+			BlockState blockState = getDefaultState().with(WATERLOGGED, wl).with(FACING, direction.getOpposite());
+			if (blockState.canPlaceAt(ctx.getWorld(), ctx.getBlockPos())) return blockState;
+		}
+		return null;
+	}
+
+	@Override
+	protected boolean canPlaceAt(BlockState state, WorldView world, BlockPos pos) {
+		BlockPos blockPos = pos.offset(state.get(FACING).getOpposite());
+		return world.getBlockState(blockPos).isSideSolidFullSquare(world, blockPos, state.get(FACING));
+	}
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
--- src/main/java/com/edryu/morethings/block/TurnTableBlock.java
+++ src/main/java/com/edryu/morethings/block/TurnTableBlock.java
@@ -1,25 +1,39 @@
 package com.edryu.morethings.block;
 
+import com.mojang.serialization.MapCodec;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.block.FacingBlock;
+import net.minecraft.item.ItemPlacementContext;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
-import net.minecraft.state.property.DirectionProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.math.Direction;
 
-public class TurnTableBlock extends Block {
-    public static final DirectionProperty FACING = Properties.FACING;
+public class TurnTableBlock extends FacingBlock {
+    public static final MapCodec<TurnTableBlock> CODEC = Block.createCodec(TurnTableBlock::new);
+
     public static final BooleanProperty INVERTED = Properties.INVERTED;
-    public static final BooleanProperty ROTATING = BooleanProperty.of("rotating");
+    public static final BooleanProperty POWERED = Properties.POWERED;
 
     public TurnTableBlock(Settings settings) {
         super(settings);
-        setDefaultState(getDefaultState().with(FACING, Direction.NORTH).with(INVERTED, false).with(ROTATING, false));
+        setDefaultState(getDefaultState().with(FACING, Direction.NORTH).with(INVERTED, false).with(POWERED, false));
     }
+
+	@Override
+	protected MapCodec<? extends TurnTableBlock> getCodec() {
+		return CODEC;
+	}
+
+	@Override
+	public BlockState getPlacementState(ItemPlacementContext ctx) {
+		return this.getDefaultState().with(FACING, ctx.getPlayerLookDirection().getOpposite());
+	}
 
     @Override
     protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-        builder.add(FACING, INVERTED, ROTATING);
+        builder.add(FACING, INVERTED, POWERED);
     }
 }
