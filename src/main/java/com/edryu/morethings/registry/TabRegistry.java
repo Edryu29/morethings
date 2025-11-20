6a86d69c9aa3eec6537ea9f058cfe5608f22b298
Edryu <trickyjuggler29@gmail.com>
2025-11-20 14:30:04 -0500

    Changed Creative Tab item order

--- src/main/java/com/edryu/morethings/registry/TabRegistry.java
+++ src/main/java/com/edryu/morethings/registry/TabRegistry.java
@@ -21,41 +21,40 @@ public class TabRegistry {
         Registry.register(BuiltInRegistries.CREATIVE_MODE_TAB, MORE_BLOCKS_GROUP_KEY, MORE_BLOCKS_GROUP);
 
         ItemGroupEvents.modifyEntriesEvent(MORE_BLOCKS_GROUP_KEY).register(itemGroup -> {
-            itemGroup.accept(BlockRegistry.BIG_CHAIN.asItem());
-            itemGroup.accept(BlockRegistry.BOOK_STACK.asItem());
-            itemGroup.accept(BlockRegistry.BOOK_PILE.asItem());
-            itemGroup.accept(BlockRegistry.BAR_PANEL.asItem());
-            itemGroup.accept(BlockRegistry.LATTICE.asItem());
+            itemGroup.accept(BlockRegistry.SACK.asItem());
+            itemGroup.accept(BlockRegistry.SAFE.asItem());
+            itemGroup.accept(BlockRegistry.DISPLAY.asItem());
+            itemGroup.accept(BlockRegistry.SMALL_PEDESTAL.asItem());
 
             itemGroup.accept(BlockRegistry.DAUB.asItem());
             itemGroup.accept(BlockRegistry.DAUB_SLAB.asItem());
             itemGroup.accept(BlockRegistry.PEDESTAL.asItem());
             itemGroup.accept(BlockRegistry.STONE_PILLAR.asItem());
 
+            itemGroup.accept(BlockRegistry.HAYSTACK.asItem());
             itemGroup.accept(BlockRegistry.THATCH.asItem());
             itemGroup.accept(BlockRegistry.THATCH_SLAB.asItem());
             itemGroup.accept(BlockRegistry.THATCH_STAIRS.asItem());
-            itemGroup.accept(BlockRegistry.HAYSTACK.asItem());
 
+            itemGroup.accept(BlockRegistry.GLOBE.asItem());
+            itemGroup.accept(BlockRegistry.GLOBE_SEPIA.asItem());
+            itemGroup.accept(BlockRegistry.BOOK_STACK.asItem());
+            itemGroup.accept(BlockRegistry.BOOK_PILE.asItem());
             itemGroup.accept(BlockRegistry.BOAT_IN_A_JAR.asItem());
             itemGroup.accept(BlockRegistry.TERRARIUM.asItem());
             itemGroup.accept(BlockRegistry.TELESCOPE.asItem());
             itemGroup.accept(BlockRegistry.SPYGLASS_STAND.asItem());
-            itemGroup.accept(BlockRegistry.GLOBE.asItem());
-            itemGroup.accept(BlockRegistry.GLOBE_SEPIA.asItem());
+
+            itemGroup.accept(BlockRegistry.BIG_CHAIN.asItem());
+            itemGroup.accept(BlockRegistry.ROPE.asItem());
+            itemGroup.accept(ItemRegistry.BUNTING);
 
             itemGroup.accept(BlockRegistry.CRANK.asItem());
-            itemGroup.accept(BlockRegistry.ROPE.asItem());
             itemGroup.accept(BlockRegistry.COG.asItem());
             itemGroup.accept(BlockRegistry.PULLEY.asItem());
             itemGroup.accept(BlockRegistry.TURN_TABLE.asItem());
             itemGroup.accept(BlockRegistry.RED_BUTTON.asItem());
             itemGroup.accept(BlockRegistry.CONSOLE.asItem());
-
-            itemGroup.accept(BlockRegistry.SACK.asItem());
-            itemGroup.accept(BlockRegistry.SAFE.asItem());
-            itemGroup.accept(BlockRegistry.SMALL_PEDESTAL.asItem());
-            itemGroup.accept(BlockRegistry.DISPLAY.asItem());
 
             itemGroup.accept(BlockRegistry.ACACIA_PALISADE.asItem());
             itemGroup.accept(BlockRegistry.BAMBOO_PALISADE.asItem());
@@ -69,6 +68,9 @@ public class TabRegistry {
             itemGroup.accept(BlockRegistry.PALE_OAK_PALISADE.asItem());
             itemGroup.accept(BlockRegistry.SPRUCE_PALISADE.asItem());
             itemGroup.accept(BlockRegistry.WARPED_PALISADE.asItem());
+            
+            itemGroup.accept(BlockRegistry.BAR_PANEL.asItem());
+            itemGroup.accept(BlockRegistry.LATTICE.asItem());
 
             itemGroup.accept(BlockRegistry.ACACIA_SHUTTER.asItem());
             itemGroup.accept(BlockRegistry.BIRCH_SHUTTER.asItem());
@@ -81,9 +83,6 @@ public class TabRegistry {
             itemGroup.accept(BlockRegistry.SPRUCE_SHUTTER.asItem());
 
             itemGroup.accept(ItemRegistry.ORB);
-
-            itemGroup.accept(ItemRegistry.BUNTING);
-
             itemGroup.accept(ItemRegistry.BLUE_CRYSTAL);
             itemGroup.accept(ItemRegistry.GREEN_CRYSTAL);
             itemGroup.accept(ItemRegistry.PURPLE_CRYSTAL);
